aa578f5a2a43a24b07303389300e52f5f933e7fa
tuanmjnh <minhtuan200990@gmail.com>
2020-03-19 08:38:31 +0700

    deploy heroku dist

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var path = require('path');
-var serveStatic = require('serve-static');
+// var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/spa"));
+// app.use(serveStatic(__dirname + "/spa"));
 var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started ' + port);
+
+app.use(`${process.env.BASE_URL}${process.env.PUBLIC_PATH}`, express.static(process.env.PUBLIC_DIR))
+
+app.get(process.env.BASE_URL, function(req, res, next) {
+  // res.render('index', { title: 'Express' })
+  res.end('TM-Store Express Server api', { title: 'Express' })
+})
+
+const server = app.listen(port) //, '192.168.1.10' // '127.0.0.1'
+  .on('listening', () => {
+    process.env.HOST = `http://${server.address().address}:${port}`
+    console.log(`Web server listening on: ${process.env.HOST}`)
+    console.log(`Mode: ${process.env.NODE_ENV}`)
+  })
+  .on('error', (err) => { console.log(err) })
